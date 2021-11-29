33e3d250944baf52251300109828cff3627619c3
Roberto D'Ambrosio <roberto8601@gmail.com>
2021-11-29 15:32:57 +0100

    js completo (no html css)

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 const app = new Vue({
     el: '#root',
     data: {
-        todo: [
-            {
+        todo: [{
                 text: 'Portare fuori il cane',
                 done: false,
             },
@@ -23,34 +22,37 @@ const app = new Vue({
                 done: false,
             },
         ],
-        inputText : '',
+        inputText: '',
     },
     methods: {
-		addTodo: function() {
-			if( this.inputText !== "" ) {
-				this.todo.push({text: this.inputText, done: false});
-				this.inputText = "";
-			}
-		},
-        removeTodo: function(i) {
-			this.todo.splice(i, 1);
-		},
-
-        lineTodo: function(element) {
-            if (element.done === true){
-                return 'sbarrato';
-               } else {
-                return '';
-                }
-                        
+        addTodo: function () {
+            if (this.inputText !== "") {
+                this.todo.push({
+                    text: this.inputText,
+                    done: false
+                });
+                this.inputText = "";
+            }
+        },
+        removeTodo: function (i) {
+            this.todo.splice(i, 1);
         },
 
-        invertDone: function(element) {
-            if(element.done == true) {
+        lineTodo: function (element) {
+            if (element.done === true) {
+                return 'sbarrato';
+            } else {
+                return '';
+            }
+
+        },
+
+        invertDone: function (element) {
+            if (element.done == true) {
                 element.done = false;
             } else {
                 element.done = true;
             }
-    },
-}
+        },
+    }
 });

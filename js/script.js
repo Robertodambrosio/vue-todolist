98bfd85b721f5dc7f5bd767f71669bed707904f4
Roberto D'Ambrosio <roberto8601@gmail.com>
2021-11-29 15:10:38 +0100

    methods done

--- js/script.js
+++ js/script.js
@@ -1,11 +1,56 @@
-const app = new Vue ({
+const app = new Vue({
     el: '#root',
-    data: 
-    {
-
+    data: {
+        todo: [
+            {
+                text: 'Portare fuori il cane',
+                done: false,
+            },
+            {
+                text: 'Andare in palestra',
+                done: true,
+            },
+            {
+                text: 'Vedere la Juve',
+                done: false,
+            },
+            {
+                text: 'Preparare la cena',
+                done: true,
+            },
+            {
+                text: 'Studiare',
+                done: false,
+            },
+        ],
+        inputText : '',
     },
-    methods: 
-    {
+    methods: {
+		addTodo: function() {
+			if( this.inputText !== "" ) {
+				this.todo.push({text: this.inputText, done: false});
+				this.inputText = "";
+			}
+		},
+        removeTodo: function(i) {
+			this.todo.splice(i, 1);
+		},
 
-    }
-})
+        lineTodo: function(element) {
+            if (element.done === true){
+                return 'sbarrato';
+               } else {
+                return '';
+                }
+                        
+        },
+
+        invertDone: function(element) {
+            if(element.done == true) {
+                element.done = false;
+            } else {
+                element.done = true;
+            }
+    },
+}
+});
